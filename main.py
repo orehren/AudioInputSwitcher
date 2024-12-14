a522a605fc79641e82fd4aa3d1b3792a65e879b3
orehren <o.rehren@gmail.com>
2024-12-14 06:17:44 +0100

    Alternating the original code to input sources

--- main.py
+++ main.py
@@ -5,46 +5,46 @@ from src.backend.DeckManagement.InputIdentifier import Input
 from src.backend.PluginManager.ActionInputSupport import ActionInputSupport
 
 # Import actions
-from .actions.SetOutput.SetOutput import SetOutput
-from .actions.ToggleOutput.ToggleOutput import ToggleOutput
+from .actions.SetInput.SetInput import SetInput
+from .actions.ToggleInput.ToggleInput import ToggleInput
 
-class AudioSwitcher(PluginBase):
+class AudioInputSwitcher(PluginBase): # Changed plugin class name
     def __init__(self):
         super().__init__()
 
         self.lm = self.locale_manager
 
         ## Register actions
-        self.set_output_holder = ActionHolder(
+        self.set_input_holder = ActionHolder( # Changed name
             plugin_base = self,
-            action_base = SetOutput,
-            action_id_suffix = "SetOutput", # Change this to your own plugin id
-            action_name = self.lm.get("actions.set-output.name"),
+            action_base = SetInput, # Changed name
+            action_id_suffix = "SetInput", # Changed suffix
+            action_name = self.lm.get("ais.actions.set-input.name"), # Changed label
             action_support={
                 Input.Key: ActionInputSupport.SUPPORTED,
                 Input.Dial: ActionInputSupport.SUPPORTED,
                 Input.Touchscreen: ActionInputSupport.UNTESTED
             }
         )
-        self.add_action_holder(self.set_output_holder)
+        self.add_action_holder(self.set_input_holder)
 
-        self.toggle_output_holder = ActionHolder(
+        self.toggle_input_holder = ActionHolder( # Changed name
             plugin_base = self,
-            action_base = ToggleOutput,
-            action_id_suffix = "ToggleOutput", # Change this to your own plugin id
-            action_name = self.lm.get("actions.toggle-output.name"),
+            action_base = ToggleInput, # Changed name
+            action_id_suffix = "ToggleInput", # Changed suffix
+            action_name = self.lm.get("ais.actions.toggle-input.name"), # Changed label
             action_support={
                 Input.Key: ActionInputSupport.SUPPORTED,
                 Input.Dial: ActionInputSupport.SUPPORTED,
                 Input.Touchscreen: ActionInputSupport.UNTESTED
             }
         )
-        self.add_action_holder(self.toggle_output_holder)
+        self.add_action_holder(self.toggle_input_holder)
 
         # Register plugin
         self.register(
             plugin_name = self.lm.get("plugin.name"),
-            github_repo = "https://github.com/StreamController/AudioSwitcher",
+            github_repo = "https://github.com/orehren/AudioInputSwitcher", # Update with your repo
             plugin_version = "1.0.0",
             app_version = "1.4.5-beta"
         )
